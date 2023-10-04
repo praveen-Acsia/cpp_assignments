c4829e654ae16e962bfa6925eded622cfadc70ca
praveen-Acsia <141117441+praveen-Acsia@users.noreply.github.com>
2023-10-04 21:38:07 +0530

    Update assignment8_q2.cpp

--- day_8/assignment8_q2.cpp
+++ day_8/assignment8_q2.cpp
@@ -1,94 +1,106 @@
 #include <iostream>
-#include <cstring>
+#include <string.h>
 
 using namespace std;
 
 class CEmployee
 {
-private:
-    const int m_nId;
-    char* m_pchName;
-public:
-    CEmployee(int , char[]) ;
-    ~CEmployee();
-    void displayPerson();
-    char* getname();
+    private:
+        int m_nId;
+        char* m_pchName;
+    public:
+        CEmployee(int nId, char* pchName);
+        ~CEmployee();
+        void displayPerson();
+
+
+        bool operator<(CEmployee obj){
+            if(m_pchName[0] < obj.m_pchName[0])
+                return true;
+            else
+                return false;
+        }
 };
+
+CEmployee::CEmployee(int nId, char* pchName) : m_nId(nId){
+    m_pchName = new char[10];
+    strcpy(m_pchName, pchName);
+}
+
+CEmployee::~CEmployee(){
+    delete[] m_pchName;
+}
+
+void CEmployee::displayPerson(){
+    std::cout << "\nName: " << m_pchName << "\tID: " << m_nId;
+}
 
 
 class Company
 {
-    CEmployee m_emp;
-    static int m_nCount;
-public:
-    Company(int , char[]) ;
-    ~Company();
-    void displayEmployees();
-    // void sortEmployees();
+    CEmployee* m_emp[100];
+    int nCount;
+    
+    public:
+        Company(int);
+        ~Company(){};
+        void swapName(Company, Company);
+        void sort();
+        void displayEmployees();
 };
 
+Company::Company(int n){
+    nCount = n;
 
-CEmployee::CEmployee(int id=0, char name[]=NULL) : m_nId(id), m_pchName(new char[10]){
-    strcpy(m_pchName, name);
-}
-void CEmployee::displayPerson(){
-    cout <<"\nId : " << m_nId << "\nName : " << m_pchName << endl;
-}
-CEmployee::~CEmployee(){
-    delete[] m_pchName;
-}
-char* CEmployee::getname(){
-    return m_pchName;
+    char name[10];
+    int Id;
+
+    
+
+    for(int i = 0; i < nCount; i++){
+        std::cout << "Name: ";
+        std::cin >> name;
+        std::cout << "\nId: ";
+        std::cin >> Id;
+
+        m_emp[i] = new CEmployee(Id, name);
+    }
+
 }
 
 
 
-Company::Company(int id=0, char name[]= NULL): m_emp(id, name){
+void Company::sort(){
+    CEmployee* temp = new CEmployee(0, "xxx");
+    for (size_t i = 0; i < nCount; i++)
+    {
+        for (size_t j = i + 1; j < nCount; j++)
+        {
+            if(m_emp[i] < m_emp[j]){
+                temp = m_emp[i];
+                m_emp[i] = m_emp[j];
+                m_emp[j] = temp;
+            }
+        }
+        
+    }
 }
+
 void Company::displayEmployees(){
-    m_emp.displayPerson();
+    for (size_t i = 0; i < nCount; i++)
+    {
+        m_emp[i]->displayPerson();
+    }
+    
 }
-Company::~Company(){
-    cout << "company destructor.\n";
-}
-// void Company::sortEmployees(){
-
-//     for(int i = 0; i < m_nCount; i++){
-//         for(int j =0 ; j < i; j++){
-//             if(m_emp.getname()[i] > m_emp.getname())
-//         }
-//     }
-// }
-
-
-int Company::m_nCount;
 
 
 int main()
 {
-    int id;
-    char name[10];
-    //Read nCount no: of employees
-    cout << "\nEnter no of employees : ";
-    cin >> m_nCount;
+    int nCount;
+    std::cin >> nCount;
 
-    //Create company which has nCount of employees.
-    Company* pCompany[m_nCount];
-
-
-    //Read details of employees in Company
-    for(int i =0; i < m_nCount; i++){
-        cout << "\nEnter employee id : ";
-        cin >> id;
-        cout << "\nEnter employee name : ";
-        cin >> name;
-        pCompany[i]=new Company(id, name);
-    }
-    //Sort employees in Company
-    // pCompany.sortEmployee();
-
-    //Print employees in Company
-    for(int i =0; i < m_nCount; i++){
-        pCompany[i]->displayEmployees();
-    }
+    Company* pCompany = new Company(nCount);
+    pCompany->sort();
+    pCompany->displayEmployees();
 }
